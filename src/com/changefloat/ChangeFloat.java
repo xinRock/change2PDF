9f21ce9befc27e515a8104643f9601c85a9f4b1e
xinRock <huanglaiye@163.com>
2019-07-10 15:20:56 +0800

    单精度浮点IEEE745标准转换

---
+++ src/com/changefloat/ChangeFloat.java
@@ -0,0 +1,20 @@
+package com.changefloat;
+
+/**
+ * 
+ * 将100.2转成IEEE745 二进制表示
+ * @author menggq
+ *
+ */
+public class ChangeFloat {
+	public static void main(String[] args) {
+		String value = convert(100.2f);
+		System.out.println(value);
+	}
+
+	public static String convert(float num) {
+		int intVal = Float.floatToIntBits(num);
+		return intVal > 0 ? "0" + Integer.toBinaryString(intVal) : Integer.toBinaryString(intVal);
+	}
+
+}
